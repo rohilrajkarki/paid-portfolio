909ba6fcd80853d1eaae30ec910a2d4c202eb681
rohilrajkarki <87067787+rohilrajkarki@users.noreply.github.com>
2021-12-07 16:06:43 +0545

    about-completed

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,11 @@
+import "./product.css"
+
+const Product = () => {
+    return (
+        <div className="p">
+            Cards
+        </div>
+    )
+}
+
+export default Product
---
+++ src/components/productlist/ProductList.jsx
@@ -0,0 +1,28 @@
+import Product from "../product/Product"
+import './productList.css';
+
+const ProductList = () => {
+    return (
+        <div className="pl">
+            <div className="pl-texts">
+                <h1 className="pl-title">🐻 Bear & Beers 🍻</h1>
+                <p className="pl-desc">
+                    Lama is a creative portfolio that your work has been waiting for.
+                    Beautiful homes, stunning portfolio styles & a whole lot more awaits
+                    inside.
+                </p>
+            </div>
+            <div className="pl-list">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+
+
+            </div>
+        </div>
+    )
+}
+
+export default ProductList
